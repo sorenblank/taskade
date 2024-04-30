49ce4df9f23e37fcc661e94b54f49586f486d6bf
sorenblank <afrozaman12@gmail.com>
2024-04-30 17:18:05 +0600

    Renamed existing .js files to .jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Taskade",
+  description: "Taskade is a simple project and task manager. Perfect for remote teams. Unleash your team productivity with projects task list and collaboration tools.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
