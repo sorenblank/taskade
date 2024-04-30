b3b5d65418799e5f1f579cb6f87285acf585ffe5
sorenblank <afrozaman12@gmail.com>
2024-04-30 23:15:13 +0600

    Added constants.jsx file with header and navigation data

---
+++ app/lib/constants.jsx
@@ -0,0 +1,22 @@
+export const header = {
+    icon: "/taskade.png",
+    title: "Taskade",
+}
+
+export const navigation = [
+    {
+        id: 1,
+        title: "Features",
+        url: "/features",
+    },
+    {
+        id: 2,
+        title: "For Teams",
+        url: "/teams",
+    },
+    {
+        id: 3,
+        title: "Pricing",
+        url: "/pricing",
+    },
+]
