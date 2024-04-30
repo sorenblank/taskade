6ba40d103a0429008bea66dd2884dafe7fbd54f3
sorenblank <afrozaman12@gmail.com>
2024-04-30 23:13:30 +0600

    Refactor fonts and colors in CSS files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 
-import fontfamily from 'tailwindcss/fontFamily'
 
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors : {
-        'text': 'var(--text)',
-        'background': 'var(--background)',
-        'primary': 'var(--primary)',
-        'secondary': 'var(--secondary)',
-        'accent': 'var(--accent)',
+        text: 'var(--text)',
+        background: 'var(--background)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)',
       },
-      fontfamily: {
-        inter: ["var(--font-inter)", 'Inter', ...fontfamily.sans],
-        graphik: ["var(--font-graphik)", 'Graphik', 'Helvetica'],
+      fontFamily: {
+        inter: "var(--font-inter)",
+        graphik: "var(--font-graphik)",
+        roboto: "var(--font-roboto)",
+        rubik: "var(--font-rubik)",
       }
     },
   },
