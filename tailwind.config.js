11bcee2549d8afe44acf9bc7b46f7bec0b0aacf5
sorenblank <afrozaman12@gmail.com>
2024-04-30 18:53:01 +0600

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import fontfamily from 'tailwindcss/fontFamily'
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +10,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors : {
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
       },
+      fontfamily: {
+        inter: ["var(--font-inter)", 'Inter', ...fontfamily.sans],
+        graphik: ["var(--font-graphik)", 'Graphik', 'Helvetica'],
+      }
     },
   },
-  plugins: [],
 };
